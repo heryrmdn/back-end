40f690ea98d8ae706023f176b60c8293c611a34b
unknown <heryrhamadan@gmail.com>
2023-11-18 12:28:39 +0700

    setup sequelize cli & db successfully connected

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const { sequelize } = require("./models");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+
+  console.log(`Server running on port ${port}`);
+});
