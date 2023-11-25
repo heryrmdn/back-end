69fe584accd2f9161680b2a2eedd5bf68b66203f
unknown <heryrhamadan@gmail.com>
2023-11-25 22:59:04 +0700

    feat: add dashboard-api update reservation

--- controllers/dashboard-api-controllers/reservation.controller.js
+++ controllers/dashboard-api-controllers/reservation.controller.js
@@ -1,15 +1,92 @@
-exports.getReservationList = (req, res, next) => {
-  res.send("getReservationList /dashboard-api/reservations");
+const { Op } = require("sequelize");
+const { Reservation, Customer, Package } = require("../../models");
+const { throwError } = require("../../utils/throw-error");
+
+exports.getReservationList = async (req, res, next) => {
+  const userId = req.user.userId;
+
+  const reservations = await Reservation.findAll({
+    attributes: ["id", "date", "status"],
+    include: {
+      model: Customer,
+      as: "customer",
+      attributes: ["name", "image"],
+    },
+    where: {
+      doctorId: userId,
+    },
+  });
+
+  return res.status(200).json({
+    status: "success",
+    code: 200,
+    message: "Success get reservation list",
+    reservations,
+  });
 };
 
-exports.getReservationDetail = (req, res, next) => {
-  const params = req.params;
+exports.getReservationDetail = async (req, res, next) => {
+  const userId = req.user.userId;
+  const paramsId = parseInt(req.params.id);
 
-  res.send(`getReservationDetail /dashboard-api/reservations/${params.id}`);
+  const reservation = await Reservation.findOne({
+    attributes: ["id", "date", "time", "status"],
+    include: [
+      {
+        model: Customer,
+        as: "customer",
+        attributes: ["name", "image"],
+      },
+      {
+        model: Package,
+        as: "package",
+        attributes: ["name", "price", "duration"],
+      },
+    ],
+    where: {
+      [Op.and]: [{ doctorId: userId }, { id: paramsId }],
+    },
+  });
+
+  if (!reservation) {
+    throwError("Data not found", 404, next);
+  } else {
+    return res.status(200).json({
+      status: "success",
+      code: 200,
+      message: "Success get reservation detail",
+      reservation,
+    });
+  }
 };
 
-exports.UpdateReservation = (req, res, next) => {
-  const params = req.params;
+exports.UpdateReservation = async (req, res, next) => {
+  const userId = req.user.userId;
+  const paramsId = parseInt(req.params.id);
+  const body = req.body;
 
-  res.send(`UpdateReservation /dashboard-api/reservations/${params.id}`);
+  const isValidReservation = await Reservation.findOne({
+    where: {
+      [Op.and]: [{ doctorId: userId }, { id: paramsId }],
+    },
+  });
+
+  if (!isValidReservation) {
+    throwError("Data not found", 404, next);
+  } else {
+    await Reservation.update(
+      { status: body.status },
+      {
+        where: {
+          [Op.and]: [{ doctorId: userId }, { id: paramsId }],
+        },
+      }
+    );
+
+    return res.status(200).json({
+      status: "success",
+      code: 200,
+      message: "Success update reservation",
+    });
+  }
 };
--- routes/dashboard-api/reservation.js
+++ routes/dashboard-api/reservation.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { getReservationList, getReservationDetail, UpdateReservation } = require("../../controllers/dashboard-api-controllers/reservation.controller");
 const { tryCatch } = require("../../utils/try-catch");
+const { authorization } = require("../../middleware/auth-middleware");
 const router = express.Router();
 
-router.get("/", tryCatch(getReservationList));
-router.get("/:id", tryCatch(getReservationDetail));
-router.put("/:id", tryCatch(UpdateReservation));
+router.get("/", authorization, tryCatch(getReservationList));
+router.get("/:id", authorization, tryCatch(getReservationDetail));
+router.put("/:id", authorization, tryCatch(UpdateReservation));
 
 module.exports = router;
